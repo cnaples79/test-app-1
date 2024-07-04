a4fc2ae129bcb3f6ee77f57067f3db5c5db99689
cnaples79 <cnaples79@gmail.com>
2024-07-04 03:02:31 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_TSC_COMPILE_ON_ERROR: process.env.NEXT_PUBLIC_TSC_COMPILE_ON_ERROR || 'false', // Default to 'false' if not set
+  },
+};
 
 export default nextConfig;
